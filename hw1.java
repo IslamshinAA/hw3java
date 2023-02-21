5180ec31a6c6655c53ba26e15908acc6e398dd45
AntonIsl <arshavin0511@gmail.com>
2023-02-21 12:42:03 +0300

    2 task ok

--- hw1.java
+++ hw1.java
@@ -1,4 +1,20 @@
 //Пусть дан произвольный список целых чисел, удалить из него четные числа
+import java.util.*;
 public class hw1 {
-    
+    public static void main(String args[]) {
+      ArrayList<Integer> list = new ArrayList();
+  for(int i = 0; i < 20; i++) {
+      list.add(i);
+  }
+  Collections.shuffle(list);
+     System.out.println(list);
+     ArrayList<Integer> list1 = new ArrayList();
+     for(int i =0; i < list.size(); i++) {
+         if(list.get(i) % 2 == 0) {
+             list1.add(list.get(i));
+             
+         }
+     }
+     System.out.println(list1);
+    }
 }
